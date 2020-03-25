014dc489a31c76b892a40480ffb1aff72b6e8eb1
Nicolas Gilles <nicolas.gilles@gmail.com>
2020-03-25 11:48:19 +0100

    Stage 1 - port binding

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    # import socket
-    # s = socket.create_server(("localhost", 6379), reuse_port=True)
-    # s.accept() # wait for client
+    import socket
+    s = socket.create_server(("localhost", 6379), reuse_port=True)
+    s.accept() # wait for client
 
 
 if __name__ == "__main__":
