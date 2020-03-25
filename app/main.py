d6fa9ce4dc8b2119eae4495c363dcd025445bda1
Nicolas Gilles <nicolas.gilles@gmail.com>
2020-03-25 12:41:38 +0100

    Stage 4 - multi-client async PONGs

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
-def main():
-    print("Implement your Redis server here!")
+import asyncio
 
-    # Uncomment this to pass the first stage
-    #
-    import socket
-    s = socket.create_server(("localhost", 6379), reuse_port=True)
-    sock, addr = s.accept() # wait for client
+async def redis_server(reader, writer):
     while True:
-        ping_data = sock.recv(4096)
-        sock.send(b'+PONG\r\n')
+        data = await reader.read(100)
+        if not data:
+            break
+        writer.write(b'+PONG\r\n')
+        await writer.drain()
+    writer.close()
 
+async def main(host, port):
+    server = await asyncio.start_server(redis_server, host, port)
+    await server.serve_forever()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main('0.0.0.0', 6379))
