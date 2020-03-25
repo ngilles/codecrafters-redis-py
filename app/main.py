5bf5771532e104cab22767a9770cca903cf01204
Nicolas Gilles <nicolas.gilles@gmail.com>
2020-03-25 12:04:33 +0100

    Stage 3 - mulitple PONGs

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ def main():
     import socket
     s = socket.create_server(("localhost", 6379), reuse_port=True)
     sock, addr = s.accept() # wait for client
-    sock.send(b'+PONG\r\n')
+    while True:
+        ping_data = sock.recv(4096)
+        sock.send(b'+PONG\r\n')
 
 
 if __name__ == "__main__":
